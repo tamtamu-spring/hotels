1facafdde9a2848185d8863a6f0b6adf9b2198b1
Mikalai Nazarenka <nazara_arhiv@mail.ru>
2017-06-04 14:47:45 +0300

    hotel entity edded

---
+++ dao/src/main/java/com/mnazarenka/dao/entity/BaseDao.java
@@ -0,0 +1,23 @@
+package com.mnazarenka.dao.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@ToString
+@MappedSuperclass
+public abstract class BaseDao implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+}
--- dao/src/main/java/com/mnazarenka/dao/entity/HotelEntity.java
+++ dao/src/main/java/com/mnazarenka/dao/entity/HotelEntity.java
@@ -8,33 +8,23 @@ import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.List;
 
 @NoArgsConstructor
 @EqualsAndHashCode(exclude = "id")
 @ToString(callSuper = true)
 @Entity
-@Table(name = "appartments")
-public class AppartmentEntity extends BaseDao implements Serializable {
-
-    /*@Getter
-    @Setter
-    private int fkKategoryId;
-    */
-    @Getter
-    @Setter
-    @ManyToOne
-    @JoinColumn(name = "fk_hotel_id", nullable = false)
-    private HotelEntity hotel;
+@Table(name = "hotels")
+public class HotelEntity extends BaseDao implements Serializable {
     @Getter
     @Setter
     @Column
     private String name;
     @Getter
     @Setter
-    @Column
-    private String description;
+    @OneToMany(mappedBy = "hotel")
+    private List<AppartmentEntity> entities;
 }
