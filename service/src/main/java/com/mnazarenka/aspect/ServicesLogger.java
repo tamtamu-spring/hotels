574f8b10e7154e7dc277eeb96ff57c93b3c82055
Mikalai Nazarenka <nazara_arhiv@mail.ru>
2017-07-14 23:29:40 +0300

    service logger was updated

--- service/src/main/java/com/mnazarenka/aspect/ServicesLogger.java
+++ service/src/main/java/com/mnazarenka/aspect/ServicesLogger.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
+
 @Aspect
 public class ServicesLogger {
     @Autowired
@@ -17,10 +19,21 @@ public class ServicesLogger {
     }
 
     @Before("services()")
-    public void loggingServiceMethod(JoinPoint JoinPoint) {
-        String packageName = JoinPoint.getTarget().getClass().getName();
-        String methodName = JoinPoint.getSignature().getName();
+    public void loggingServiceMethod(JoinPoint joinPoint) {
+        String packageName = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
 
-        logger.info(String.format("method %s from %s was called ", methodName, packageName));
+        if (args.length > 0) {
+
+            StringBuilder sb = new StringBuilder();
+            Arrays.stream(args).forEach(a -> sb.append(a).append(", "));
+            String formattedArguments = sb.toString();
+
+            logger.info(String.format("method %s from %s was called with parameters: %s.", methodName, packageName, formattedArguments.substring(0, formattedArguments.length() - 2)));
+        } else {
+
+            logger.info(String.format("method %s from %s was called without parameters", methodName, packageName));
+        }
     }
 }
