66b2f930a94c26e0251f4c8e9574aa06647d902d
Mikalai Nazarenka <nazara_arhiv@mail.ru>
2017-07-18 17:32:58 +0300

    servise tests was edded

--- service/src/test/java/com/mnazarenka/service/OrderServiceTest.java
+++ service/src/test/java/com/mnazarenka/service/OrderServiceTest.java
@@ -1,7 +1,10 @@
 package com.mnazarenka.service;
 
 import com.mnazarenka.dao.entity.AppartmentOrder;
+import com.mnazarenka.dao.entity.LuxAppartment;
+import com.mnazarenka.dao.entity.Role;
 import com.mnazarenka.dao.enums.Status;
+import com.mnazarenka.dao.utils.TestDataImporterImpl;
 import lombok.Getter;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class OrderServiceTest extends BaseServiceTest<AppartmentOrder>{
@@ -18,10 +22,50 @@ public class OrderServiceTest extends BaseServiceTest<AppartmentOrder>{
     @Getter
     private OrderService service;
 
+
     @Override
     public void testFindAll() {
         List<AppartmentOrder> orders = service.findAll();
         assertThat(orders, hasSize(2));
     }
 
+   /* @Test
+    public void testChangeStatus(){
+        AppartmentOrder appartmentOrder = service.create(new AppartmentOrder());
+        Long id = appartmentOrder.getId();
+
+        service.changeStatus(Status.CONFIRMED, 1L, 1L, appartmentOrder);
+
+        AppartmentOrder updatedOrder = service.find(id);
+
+        assertEquals(updatedOrder.getStatus(), Status.CONFIRMED);
+    }
+
+    @Test
+    public void testUpdateOrder(){
+        AppartmentOrder appartmentOrder = service.find(1L);
+        appartmentOrder.setStartDate(LocalDate.MAX);
+        service.updateOrder(1L, 1L, appartmentOrder);
+
+        AppartmentOrder updatedOrder = service.find(1L);
+
+        assertEquals(updatedOrder.getStartDate(), LocalDate.MAX);
+    }
+
+    @Test
+    public void testFindAllByUserId(){
+        List<AppartmentOrder> orders = service.findAllByUserId(1L);
+
+        assertThat(orders, hasSize(1));
+    }
+
+    @Test
+    public void testCreateOrder(){
+        AppartmentOrder order = service.createOrder(LuxAppartment.class, 1L, "UserLogin", new AppartmentOrder());
+
+        AppartmentOrder createdOrder = service.find(order.getId());
+
+        assertNotNull(createdOrder);
+    }
+*/
 }
